baa0e8969f8ab002b61374ed6500cf7b3fc2ade1
Eryczal <79263592+Eryczal@users.noreply.github.com>
2025-02-25 21:59:42 +0100

    Change tabs to spaces

--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -1,7 +1,7 @@
 function insertHTML(id, template, args, position = "beforeend") {
-	let result = template.replace(/%([a-zA-Z]+)/g, (match, key) => args[key]);
+    let result = template.replace(/%([a-zA-Z]+)/g, (match, key) => args[key]);
 
-	document.getElementById(id).insertAdjacentHTML(position, result);
+    document.getElementById(id).insertAdjacentHTML(position, result);
 }
 
 export { insertHTML };
