7a0c210e315809fe0464d54dba26a8dba31bf8a8
Eryk Holipka <79263592+Eryczal@users.noreply.github.com>
2025-03-15 22:59:00 +0100

    Add settings

--- assets/scripts/templates/eventTemplate.js
+++ assets/scripts/templates/eventTemplate.js
@@ -1,12 +1,19 @@
 const eventTemplate = (container, data) => {
     const template = `
         <div class="event-container" id="event-${data.id}">
-            <div class="event-top-info" id="event-top-info-${data.id}">
-                <header>
-                    <h2>${data.name}</h2>
-                </header>
+            <div class="event-content-container">
+                <div class="event-top-info" id="event-top-info-${data.id}">
+                    <header>
+                        <h2>${data.name}</h2>
+                    </header>
+                </div>
+                <div class="event-bar-container" id="event-bar-container-${data.id}"></div>
             </div>
-            <div class="event-bar-container" id="event-bar-container-${data.id}"></div>
+            <div class="settings-button" id="event-settings-button-${data.id}">
+                <div class="setting-dot"></div>
+                <div class="setting-dot"></div>
+                <div class="setting-dot"></div>
+            </div>
         </div>
     `;
 
