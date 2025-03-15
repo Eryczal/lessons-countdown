3cd128a4f21500e10a896558aa82efa2d535cc8a
Eryk Holipka <79263592+Eryczal@users.noreply.github.com>
2025-03-15 13:13:45 +0100

    Display events data

--- assets/scripts/events/CountdownEventsHolder.js
+++ assets/scripts/events/CountdownEventsHolder.js
@@ -1,20 +1,25 @@
 import { CountdownEvent } from "./CountdownEvent.js";
-import { differenceTemplate } from "../templates/differenceTemplate.js";
+import { eventContentTemplate } from "../templates/eventContentTemplate.js";
+import { eventTemplate } from "../templates/eventTemplate.js";
 
 class CountdownEventsHolder {
     constructor() {
-        this.events = [new CountdownEvent("test", new Date(), 360, true, 0), new CountdownEvent("test2", new Date("2025-05-21 17:00"), 50, true, 1)];
+        this.events = [
+            new CountdownEvent(0, "test", new Date("2025-02-18 19:00"), new Date(), 360, true, 0),
+            new CountdownEvent(1, "test2", new Date("2025-02-21 17:00"), new Date("2025-05-21 17:00"), 50, true, 1),
+        ];
 
         this.loadEvents();
-        this.timer = setInterval(() => this.showEvents(), 100);
+        this.showEvents();
+        this.timer = setInterval(() => this.updateEvents(), 100);
     }
 
     loadEvents() {
         if (localStorage.getItem("events") !== null) {
             let data = JSON.parse(localStorage.getItem("events"));
 
-            data.forEach((event) => {
-                this.events[i] = new CountdownEvent(event.name, event.startDate, event.duration, event.repeating, event.color);
+            data.forEach((event, i) => {
+                this.events[i] = new CountdownEvent(i, event.name, event.startDate, event.duration, event.repeating, event.color);
             });
         }
     }
@@ -22,14 +27,15 @@ class CountdownEventsHolder {
     showEvents() {
         document.getElementById("main").innerHTML = this.events
             .map((event) => {
-                const data = {
-                    diff: event.getTimeDifference(),
-                    started: event.started(),
-                };
-
-                return differenceTemplate(data);
+                return eventTemplate(event.getData());
             })
             .join("");
+    }
+
+    updateEvents() {
+        this.events.forEach((event) => {
+            document.getElementById(`event-content-${event.id}`).innerHTML = eventContentTemplate(event.getData());
+        });
     }
 }
 
