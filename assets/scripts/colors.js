222fb2223394be09e0a5221a9cfe644ebbf36ef9
Eryk Holipka <79263592+Eryczal@users.noreply.github.com>
2025-03-15 13:52:32 +0100

    Delete old files

--- assets/scripts/colors.js
+++
@@ -1,9 +0,0 @@
-const colors = [
-	["#0f0", "#ff0", "#f00"],
-	["#bdc3c7", "#2c3e50"],
-	["#fdbb2d", "#b21f1f", "#1a2a6c"],
-	["#00ceff", "#0052ff", "#4800ff", "#ef00ff", "#ff0000"],
-	["#d2d9ff", "#0024ff"],
-];
-
-export default colors;
