0af8adf66fe2459171343e4011e235f1f3c8bb27
Eryk Holipka <79263592+Eryczal@users.noreply.github.com>
2025-03-16 10:51:36 +0100

    Add countdown dialog

---
+++ assets/scripts/templates/editEventTemplate.js
@@ -0,0 +1,17 @@
+const editEventTemplate = (data, eventsHolder) => {
+    let event = {
+        id: data?.id || eventsHolder.getFreeId(),
+        name: data?.name || "",
+        creationDate: data?.creationDate || new Date(),
+        startDate: data?.startDate || new Date(),
+        duration: data?.duration || 60,
+        repeating: data?.repeating || true,
+        color: data?.color || 0,
+    };
+
+    const dialog = document.getElementById("dialog");
+
+    dialog.showModal();
+};
+
+export { editEventTemplate };
